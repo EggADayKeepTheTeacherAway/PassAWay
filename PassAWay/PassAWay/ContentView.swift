37f55a6051d79bad76176749562b1103740fb6a1
Nunthapop Nganiam <nuntha_pop@hotmail.com>
2026-05-09 18:36:22 +0700

    chore: initialize base Xcode project

---
+++ PassAWay/PassAWay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PassAWay
+//
+//  Created by Nunthapop on 9/5/2569 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PassAWay/PassAWay/PassAWayApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassAWayApp.swift
+//  PassAWay
+//
+//  Created by Nunthapop on 9/5/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct PassAWayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
