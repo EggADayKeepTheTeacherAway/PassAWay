9ccfd5afa1053147d213e028a72d91caeeb40d1c
Rattanan RUNG-UTHAI <japanrisesgamer@gmail.com>
2026-05-11 02:21:36 +0700

    chore: update schema

---
+++ PassAWay/PassAWay/Models/Item.swift
@@ -0,0 +1,24 @@
+//
+//  Item.swift
+//  PassAWay
+//
+//  Created by SHARK 🦈 on 10/5/26.
+//
+
+import Foundation
+import FirebaseFirestore
+
+
+struct Item: Identifiable, Codable {
+    @DocumentID var id: String?
+    var giverId: String
+    var photoUrl: String
+    var title: String
+    var description: String
+    var category: String
+    var condition: String
+    var pickUpArea: String
+    var status: String
+    var claimedBy: String?
+    var createdAt: Timestamp
+}
